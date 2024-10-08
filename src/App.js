5a356ed509bb7dc7b681ea549b152f9b269ade30
Elizabeth <elizabethmariyashibu2004@gmail.com>
2024-10-09 00:21:43 +0530

    view users

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ const App = () => {
           }
         />
         <Route
-          path="/viewuser"
+          path="/viewusers"
           element={
             <AuthenticatedLayout>
               <ViewUser />
