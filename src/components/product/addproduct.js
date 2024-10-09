6c46c6708b3321a81b1ddde23be3aaf3622a0d60
Elizabeth <elizabethmariyashibu2004@gmail.com>
2024-10-09 07:20:09 +0530

    adjusted the icons for delete and update ,adjusted the modals for all views,adjusted the tables

--- src/components/product/addproduct.js
+++ src/components/product/addproduct.js
@@ -98,7 +98,7 @@ const AddProduct = ({ onProductAdded }) => { // Added onProductAdded prop
               value={product.productName}
               onChange={handleChange}
               required
-             placeholder="Enter supplier name"
+             placeholder="Enter product name"
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             />
           </div>
